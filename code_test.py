140f9ad9a11a98d1bdb5b91999d6049948446898
rainmo <arronandlucky@163.com>
2015-04-16 23:58:06 +0800

    learn loop

---
+++ code_test.py
@@ -0,0 +1,6 @@
+print ('3')
+anyinput = int(input())
+while anyinput == 3:
+	print ('a')
+	anyinput = int(input())
+print ('d')
---
+++ countdown_2.py
@@ -0,0 +1,9 @@
+import time
+
+for i in range(10, 0, -1):
+	print (i, '*' * i)
+	time.sleep(1)
+
+for j in range(0, 11, 1):
+	print (j, '*' * j)
+	time.sleep(1)
