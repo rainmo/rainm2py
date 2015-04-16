621f51f313f45f34a476f750cccc0d7aff260471
rainmo <arronandlucky@163.com>
2015-04-16 14:58:45 +0800

    Create football_girl.py

---
+++ football_girl.py
@@ -0,0 +1,7 @@
+gender = input('please enter your gender:')
+if gender == 'm':
+    print ('pass')
+elif gender == 'f':
+    age = int(input('please enter your age:'))
+    if 10 <= age <= 12:
+        print ('well down')
