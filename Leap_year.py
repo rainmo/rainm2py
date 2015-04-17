a96f05221e6e34b17c87a1b01479bd24f870f2aa
rainmo <arronandlucky@163.com>
2015-04-17 19:40:30 +0800

    learn how to use comment

--- Leap_year.py
+++ Leap_year.py
@@ -1,4 +1,7 @@
 # leap year
+# 判断一个年份是闰年的依据
+# 能同时被4和100整除
+# 或者被400整除
 year = float(input('please input years:'))
 if (year % 4 == 0 and year % 100 != 0) or (year % 400) == 0:
     print ('leap year')
