c20f358304191ae6efe128175d7cafccfbe8be79
rainmo <arronandlucky@163.com>
2015-04-23 11:04:19 +0800

    update files

---
+++ 3_5_blow_1000.py
@@ -0,0 +1,12 @@
+sum_1 = 0
+sum_2 = 0
+for i in range(1, 1000):
+    if i % 3 == 0 and i % 5 != 0:    	
+        sum_1 += i
+
+for j in range(1, 1000):
+    if j % 5 == 0:        
+        sum_2 += j
+
+sum = sum_1 + sum_2
+print (sum)
--- code_test.py
+++ code_test.py
@@ -1,3 +1,6 @@
-import pygame
-pygame.init()
-screen = pygame.display.set_mode([640, 480])
+for i in range(999999, 10000000):
+    if i % 11 == 0 and i % 12 == 0 and i % 13 == 0 and i % 14 == 0 and i % 15 == 0 and i % 16 == 0 and i % 17 == 0 and i % 18 == 0 and i % 19 == 0 and i % 20 == 0:
+        print (i)
+        break
+    else:
+        print ('*')
