a2926c190c3a299071b59fb2679ab5d5b486cc0c
rainmo <arronandlucky@163.com>
2015-04-25 19:55:41 +0800

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,7 +4,7 @@
 # 或者被400整除
 year = float(input('please input years:'))
 def leap_year(year)
-if (year % 4 == 0 and year % 100 != 0) or (year % 400) == 0:
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print ('leap year')
 else:
     print ('common year')
