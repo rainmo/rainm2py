3f0500c5ec92a25fe3bccff206f96459cb32b5aa
rainmo <arronandlucky@163.com>
2015-04-25 19:54:52 +0800

    Update and rename Leap_year.py to leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,9 +1,12 @@
 # leap year
-# 判断一个年份是闰年的依据
+# 判断一个年份是闰年
 # 能同时被4和100整除
 # 或者被400整除
 year = float(input('please input years:'))
+def leap_year(year)
 if (year % 4 == 0 and year % 100 != 0) or (year % 400) == 0:
     print ('leap year')
 else:
     print ('common year')
+
+leap_year(float(input('please input years:')))
