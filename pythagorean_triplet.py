ddd8f3045e6f61e5a325dfe6e1487b989fe201a4
rainmo <arronandlucky@163.com>
2015-04-26 19:50:20 +0800

    Create pythagorean_triplet.py

---
+++ pythagorean_triplet.py
@@ -0,0 +1,11 @@
+# 找出唯一满足a + b + c = 1000的毕达哥拉斯三元组
+# a ** 2 + b **　2 = c ** 2
+for c in range(1000):
+    for b in range(1000):
+        for a in range(1000):
+            if a < b < c:
+                if c == (a ** 2 + b ** 2) ** 0.5:
+                    if c == 1000 - a - b:
+                        print (a, b, c)
+                        d = a * b * c
+                        print (d)
