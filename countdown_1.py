8e1cb4bbb4085c04b5707cb0a61ebd6eb84ee4f9
rainmo <arronandlucky@163.com>
2015-05-07 10:53:54 +0800

    Update countdown_1.py

--- countdown_1.py
+++ countdown_1.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# 制作一个倒计时程序
 import time
-your_time = int(input("请输入倒计时数："))
+your_time = int(input("请输入倒计时数：")) #输入计时时长
 for i in range(your_time,0,-1):
     j = 0
-    while j < i:        
+    while j < i:               # 条件循环
         j = j + 1    
     print (i, '*' * i)
     time.sleep(1)
