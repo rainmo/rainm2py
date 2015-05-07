53e9bbbfe7efa61be553c8c8b5688481e3f09481
rainmo <arronandlucky@163.com>
2015-05-07 12:59:44 +0800

    Delete ice_cream1.py

--- ice_cream1.py
+++
@@ -1,4 +0,0 @@
-import easygui
-flavor = easygui.choicebox('What is your favorite ice cream flavor?',
-                           choices = ['Vanilla', 'Chocolate', 'Strawberry'])
-easygui.msgbox('You picked ' + flavor)
