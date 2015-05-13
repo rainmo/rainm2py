5f137c87ae6d73d458521beb595e13146cc971e3
rainmo <arronandlucky@163.com>
2015-05-13 23:03:48 +0800

    learn turtle

--- code_test.py
+++ code_test.py
@@ -1,11 +1,6 @@
-import pygame,sys
-pygame.init()
-pygame.mixer.init()
-screen=pygame.display.set_mode([640,480])
-pygame.time.delay(1000)
-pygame.mixer.music.load("bon_jovi.mp3")
-pygame.mixer.music.play()
-while 1:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            sys.exit()
+fred = '''
+
+i'm arren, are u "ok"
+
+    '''
+print (fred)
