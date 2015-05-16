b0e9bc2ceffe3a503d3c57367c4bd8d021bec553
rainmo <arronandlucky@163.com>
2015-05-16 23:37:54 +0800

    learn turtle module and class

---
+++ giraffes_class.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Animals():
+	pass
+
+class Mammals(Animals):
+	def left_Foot_Forward(self):
+		print ('left foot forward')
+
+	def left_Foot_Back(self):
+		print ('left foot back')
+
+	def Right_Foot_Forward(self):
+		print ('left foot forward')
+
+	def Right_Foot_Back(self):
+		print ('left foot back')
+
+class Giraffes(Mammals):
+	def dance(self):
+		self.left_Foot_Forward()
+		self.left_Foot_Back()
+		self.Right_Foot_Forward()
+		self.Right_Foot_Back()
+		self.left_Foot_Back()
+		self.Right_Foot_Back()
+		self.Right_Foot_Forward()
+		self.left_Foot_Forward()
+
+	
+
+reginald = Giraffes()
+reginald.dance()
+
+
+
+		
