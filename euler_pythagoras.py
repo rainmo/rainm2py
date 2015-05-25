91826764fbecd5bb158ae9dae597f61e49bb4ae7
rainmo <arronandlucky@163.com>
2015-05-25 16:48:41 +0800

    Update euler_pythagoras.py

--- euler_pythagoras.py
+++ euler_pythagoras.py
@@ -13,3 +13,16 @@ for c in range(1000):
                         print (a, b, c)
                         d = a * b * c
                         print (d)
+                        
+# ++++++++++++++++++++++++++++++++++++++++++++++++
+
+#!/usr/bin/python
+
+c=0
+while True:
+	for b in range(0,c):
+		for a in range(0,b):
+			if a**2 + b**2 == c**2:
+				if a+b+c == 1000:
+					print （"a = %d, b = %d, c = %d a * b * c = %d" % (a, b, c,a * b * c)）
+	c+=1
