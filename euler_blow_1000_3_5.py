bcbee1282c138be46430706da335d52f5f06375d
rainmo <arronandlucky@163.com>
2015-05-27 12:50:04 +0800

    Update euler_blow_1000_3_5.py

--- euler_blow_1000_3_5.py
+++ euler_blow_1000_3_5.py
@@ -13,3 +13,15 @@ for j in range(1, 1000):
 
 sum = sum_1 + sum_2
 print (sum)
+
+# http://www.jb51.net/article/59663.htm
+'''
+问题描述：假设我有这样两个list，
+          一个是list1，list1 = [1, 2, 3, 4, 5]
+          一个是list2，list2 = [1, 4, 5] 
+          我们如何得到一个新的list，list3，
+          list3中包括所有不在list2中出现的list1中的元素。
+          即：list3 = list1 – list2
+          
+解决方案：我们可以用set（集合）操作
+          list3 = list(set(list1) – set(list2))
