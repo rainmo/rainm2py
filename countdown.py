ddac1400a0b2640a7725198922a4b3081530a863
rainmo <arronandlucky@163.com>
2015-06-06 15:23:01 +0800

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 # 制作一个倒计时程序
 import time
+
 your_time = int(input("请输入倒计时数：")) #输入计时时长
-for i in range(your_time,0,-1):
+for i in range(your_time, 0, -1):
     j = 0
     while j < i:               # 条件循环
         j = j + 1    
