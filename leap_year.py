26a984858fb735d5488e5bd00970c119c7d4b95a
rainmo <arronandlucky@163.com>
2015-06-07 10:36:24 +0800

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # leap year
 # 判断一个年份是闰年
 # 能同时被4和100整除
 # 或者被400整除
+
 def leap_year(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print ('leap year')
