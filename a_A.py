ac79da0c2b289927301e5aa5aacf6cb506a0f976
rainmo <arronandlucky@163.com>
2015-06-07 20:39:12 +0800

    Update a_A.py

--- a_A.py
+++ a_A.py
@@ -5,13 +5,16 @@
 # 下面的程序是将输入的名字的首字母转化成大写字母后输出
 # ***************************
 
-n = 3
-lst = []
-while n > 0:
-    name = input('please enter your name:')
-    new_name = name.capitalize()
-    lst.append(new_name)
-    n -= 1
-print (lst)
-
+def a_2_A():
+	while True:
+		name = input('please enter your name:')
+		new_name = name.capitalize()
+		print (new_name)
+		continue_type = input('Do you need continue type?(Y/N)')
+		if continue_type == 'Y' or 'y':
+			continue
+		elif continue_type == 'N' or 'n':
+			break
+		
+print (a_2_A())
     
