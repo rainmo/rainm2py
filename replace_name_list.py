36ce76bdfe02aa46915929c2f3de830acdd1517a
rainmo <arronandlucky@163.com>
2015-06-08 10:45:12 +0800

    Update replace_name_list.py

--- replace_name_list.py
+++ replace_name_list.py
@@ -5,45 +5,43 @@
 
 import sys
 
-print ('Enter 5 names:')
+name_list = []
+n = 5
+print ('Enter 5 name_list:')
+while n > 0:
+    type_name = input()
+    name_list.append(type_name)
+    n -= 1
+print ('The name_list are:', name_list)
 
-a = input()
-b = input()
-c = input()
-d = input()
-e = input()
-
-names = [a, b, c, d, e]
-print ('The names are:', a, b, c, d, e)
 rep = int(input('Replace one name. Which one? (1-5):'))
 if rep == 1:
-    del names[0]
-    names.insert(0, input('New name:'))
-    print ('The names are:', end=' ')
-    for i in names:
+    del name_list[0]
+    name_list.insert(0, input('New name:'))
+    print ('The name_list are:', end=' ')
+    for i in name_list:
         print (i, end=' ')
 elif rep == 2:
-    del names[1]
-    names.insert(1, input('New name:'))
-    print ('The names are:', end=' ')
-    for i in names:
+    del name_list[1]
+    name_list.insert(1, input('New name:'))
+    print ('The name_list are:', end=' ')
+    for i in name_list:
         print (i, end=' ')
 elif rep == 3:
-    del names[2]
-    names.insert(2, input('New name:'))
-    print ('The names are:', end=' ')
-    for i in names:
+    del name_list[2]
+    name_list.insert(2, input('New name:'))
+    print ('The name_list are:', end=' ')
+    for i in name_list:
         print (i, end=' ')
 elif rep == 4:
-    del names[3]
-    names.insert(3, input('New name:'))
-    print ('The names are:', end=' ')
-    for i in names:
+    del name_list[3]
+    name_list.insert(3, input('New name:'))
+    print ('The name_list are:', end=' ')
+    for i in name_list:
         print (i, end=' ')
 elif rep == 5:
-    del names[4]
-    names.insert(4, input('New name:'))
-    print ('The names are:', end=' ')
-    for i in names:
+    del name_list[4]
+    name_list.insert(4, input('New name:'))
+    print ('The name_list are:', end=' ')
+    for i in name_list:
         print (i, end=' ')
-    
