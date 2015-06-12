2dd54f2a2b96b3f93749655b74dd87ea2abfb9df
rainmo <arronandlucky@163.com>
2015-06-12 20:39:27 +0800

    Update one_unknown_equation.py

--- one_unknown_equation.py
+++ one_unknown_equation.py
@@ -4,16 +4,14 @@
 import math
 
 def quadratic(a, b, c):
-	delta = b ** 2 - 4 * a * c  # delta △ 快捷键：alt+41463
-	if delta > 0:
-		x_1 = -b + math.sqrt(delta)
-		x_2 = -b - math.sqrt(delta)
-		
-	elif delta == 0:
-		x_1 = - (b / (2 * a))
-		x_2 = x_1
-		
-	elif delta < 0:
-		print ('No solution')
-
-	return x_1, x_2
+    delta = b ** 2 - 4 * a * c  # delta △ 快捷键：alt+41463
+    if delta > 0:
+        x_1 = -b + math.sqrt(delta)
+        x_2 = -b - math.sqrt(delta)
+        return x_1, x_2
+    elif delta == 0:
+        x_1 = -(b / (2 * a))
+        x_2 = x_1
+        return x_1, x_2
+    elif delta < 0:
+        print ('No solution')
