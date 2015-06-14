4aab8931a35ced00edcb42b304bff1ac16a67f44
rainmo <arronandlucky@163.com>
2015-06-14 15:05:31 +0800

    Create compare_num.py

---
+++ compare_num.py
@@ -0,0 +1,20 @@
+import random
+
+def compare(type_num):   
+    secret = random.randint(1, 100)
+    print('secret =', secret)
+    if secret == type_num:
+        print('equal')
+    elif secret > type_num:
+        print('too low')
+    else:
+        print('too high')
+                  
+while True:            
+    type_num = int(input())
+    compare(type_num)
+    ask_people = input('Do you need next compare?(y/n):')
+    if ask_people == 'y':
+        continue
+    elif ask_people == 'n':
+        break
