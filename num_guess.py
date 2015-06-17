a76ef8b6b05fd40043573904448b880f1c9cb589
rainmo <arronandlucky@163.com>
2015-06-17 22:39:12 +0800

    Update num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -9,8 +9,8 @@ secret = random.randint(1, 100)
 guess = 0
 tries = 0
 
-print ('AHOY! I\'m the Dread Pirate Robers, and I have a secret!')
-print ('It is a number from 1 to 99. I\'ll give you 6 tries.')
+print('AHOY! I\'m the Dread Pirate Robers, and I have a secret!')
+print('It is a number from 1 to 99. I\'ll give you 6 tries.')
 
 while guess != secret and tries <6:
 	guess = int(input('What\'s your guess?'))
@@ -21,5 +21,5 @@ while guess != secret and tries <6:
 	tries += 1
 
 if guess == secret:
-	print ('Bingo! You got it！ Found my secret, you did!')
-	print ('The secret number was:', secret)
+	print('Bingo! You got it！ Found my secret, you did!')
+	print('The secret number was:', secret)
