65afeb7b897ca3c1d1906f92b88c846ce0a5fe76
rainmo <arronandlucky@163.com>
2015-06-18 14:59:17 +0800

    Update ancient_age.py

--- ancient_age.py
+++ ancient_age.py
@@ -3,38 +3,34 @@
 
 # 古代对不同年纪的称谓
 
-age = int(input('请输入您的年龄：'))
-if age <= 1:
-    print ('襁褓')
-elif 2 <= age <= 3:
-    print ('孩提')
-elif age == 7:
-    print ('髫年')
-elif age == 8:
-    print ('童龀[chèn], 总角')
-elif age == 9:
-    print ('九龄')
-elif age <= 10:
-    print ('黄口')
-elif age == 12:
-    print ('金钗之年')
-elif age == 13:
-    print ('豆蔻年华')
-elif age >= 13:
-    print ('舞勺之年')
-elif age == 15:
-    print ('及笄之年')
-elif age == 16:
-    print ('碧玉年华')
-elif age > 15 and age < 20:
-    print ('舞象之年')
-elif age == 20:
-    print ('桃李年华')
-elif age >= 21:
-    print ('弱冠')
-elif age > 15 and age < 20:
-    print ('舞象之年')
-elif age == 20:
-    print ('桃李年华')
-elif age >= 21:
-    print ('弱冠')
+def ancient_age(age):
+    if age <= 1:
+        print ('襁褓')
+    elif 2 <= age <= 3:
+        print ('孩提')
+    elif age == 7:
+        print ('髫年')
+    elif age == 8:
+        print ('童龀[chèn], 总角')
+    elif age == 9:
+        print ('九龄')
+    elif age <= 10:
+        print ('黄口')
+    elif age == 12:
+        print ('金钗之年')
+    elif age == 13:
+        print ('豆蔻年华')
+    elif age >= 13:
+        print ('舞勺之年')
+    elif age == 15:
+        print ('及笄之年')
+    elif age == 16:
+        print ('碧玉年华')
+    elif age > 15 and age < 20:
+        print ('舞象之年')
+    elif age == 20:
+        print ('桃李年华')
+    elif age >= 21:
+        print ('弱冠')
+
+ancient_age(age = int(input('请输入您的年龄：')))
