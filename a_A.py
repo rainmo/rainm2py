df1a49dd908f747c4547a2b7e0564457f804d129
rainmo <arronandlucky@163.com>
2015-06-22 18:19:11 +0800

    Update a_A.py

--- a_A.py
+++ a_A.py
@@ -16,3 +16,14 @@ while True:
 		continue
 	elif continue_type =='N' or continue_type =='n':
 		break
+
+# ***************************
+# 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，
+# 其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']
+def normalize(name):		
+	new_name = name.capitalize()
+	return new_name
+
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
