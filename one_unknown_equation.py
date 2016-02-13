a667e5688d9252bd4dc605d81fab47270b9189e1
rainmo <arronandlucky@163.com>
2016-02-13 16:35:01 +0800

    Update one_unknown_equation.py

--- one_unknown_equation.py
+++ one_unknown_equation.py
@@ -17,3 +17,22 @@ def quadratic(a, b, c):
         return x_1, x_2
     elif delta < 0:
         print ('No solution')
+        
+###################################################################
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import math
+
+def quadratic(a, b, c):
+	if delta == 0:
+		return -b
+	if delta < 0:
+		return None
+	else:
+		return -b + math.sqrt(delta), -b - math.sqrt(delta)
+		
+a = int(input('please enter a: '))
+b = int(input('please enter b: '))
+c = int(input('please enter c: '))
+delta = b * b - 4 * a * c
+print(quadratic(a, b, c))
